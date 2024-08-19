4611788403e3c81f5645aa49d521fc1b68c81097
Huzaifa-1100 <asadhussainshad@gmail.com>
2024-08-20 02:00:35 +0500

    responsive imporvement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,21 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-gray-100 text-black h-screen flex items-center justify-center md:flex-wrap">
+    <div className="bg-gray-100 text-black h-screen grid grid-cols-1 px-center items-center justify-center sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2">
       {/* left side */}
-      <div className="text-3xl w-1/2 px-7">
+      <div className="text-3xl w-1/1 px-7">
         <Image
           src={"/pictures/facebookLogo.svg"}
           width={300}
           height={100}
           alt="facebook"
         />
-        <p className="ml-8 -mt-2">
+        <p className="sm:block hidden ml-8 -mt-2">
           Facebook helps you connect and share with the people in your life.
         </p>
       </div>
       {/* right side */}
-      <div className="bg-white flex flex-col p-5 rounded-xl w-1/3 shadow-md shadow-gray-400">
+      <div className=" bg-white grid grid-cols-1  p-5 rounded-xl w-1/1 shadow-md shadow-gray-400 md:w-2/3 lg:w-2/3  ">
         <input
           className="my-2 border border-1 border-gray-200 p-3 rounded-md focus:outline-1 outline-blue-500"
           type="text"
@@ -30,11 +30,15 @@ export default function Home() {
         <button className="bg-blue-500 py-2 rounded-md font-bold text-white my-2 hover:bg-blue-700 ">
           Log in
         </button>
-        <p className="text-blue-500 cursor-pointer text-center hover:underline">Forgotten password?</p>
+        <p className="text-blue-500 cursor-pointer text-center hover:underline">
+          Forgotten password?
+        </p>
         <span className="my-3">
           <hr></hr>
         </span>
-        <button className="bg-green-500 text-white font-bold m-4 py-3 w-fit px-2 mx-auto rounded-md text-[20px] hover:bg-green-700 ">Create new account</button>
+        <button className="bg-green-500 text-white font-bold m-4 py-3 w-fit px-2 mx-auto rounded-md text-[20px] hover:bg-green-700 ">
+          Create new account
+        </button>
       </div>
     </div>
   );
